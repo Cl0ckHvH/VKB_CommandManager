01a011a1be109ef7269d981ca4ea66667115c595
Cl0ck <62774712+Cl0ckHvH@users.noreply.github.com>
2022-09-19 14:36:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,28 +170,6 @@ async def show_help(message: Message):
         message_id=message.id
     )
 
-################ Функции для настройки конфига ######################################################################
-
-# Функция для удаление любого параметра переменной из конфига
-async def config_remove_item(parameter):
-    f = open("config.toml", "r", encoding="utf-8")
-    config_container = f.read()
-    temp_config = config_container.replace(config[parameter], "")
-    with open("config.toml", "w", encoding="utf-8") as f:
-        f.write(temp_config)
-    config[parameter] = ""
-
-# Функция для удаление любого параметра переменной из конфига
-async def config_edit_item(parameter, args):
-    f = open("config.toml", "r", encoding="utf-8")
-    config_container = f.read()
-    temp_replace1 = parameter + " = \"" + str(config[parameter]) + "\""
-    temp_replace2 = parameter + " = \"" + str(args) + "\""
-    temp_config = config_container.replace(temp_replace1, temp_replace2)
-    with open("config.toml", "w", encoding="utf-8") as f:
-        f.write(temp_config)
-    config[parameter] = args
-
 ################ Всё что ниже - настройка запуска и бота ############################################################
 def set_apis_from_config(apies):
     temp_token = ""
