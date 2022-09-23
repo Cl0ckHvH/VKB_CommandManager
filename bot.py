164466a9bfd8ca3997a3523d0ffa029bd616208a
Cl0ck <62774712+Cl0ckHvH@users.noreply.github.com>
2022-09-24 00:59:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,7 @@ import os
 import requests
 import sys
 
-from typing import Union
-from typing import Tuple
+from typing import Union, Tuple, Optional
 
 from vkbottle import API, VKAPIError
 from vkbottle.user import User, Message, run_multibot
@@ -62,21 +61,21 @@ async def edit_message(message: Message):
         )
 
 # Опубликовывает файл на сервер и редактирует сообщение в файл
-async def upload_and_edit_message_in_file(message, null: int = 0, false: int = 0, true: int = 1):
+async def upload_and_edit_message_in_file(message, file_name, null: int = 0, false: int = 0, true: int = 1):
     upload_server = dict(await message.ctx_api.docs.get_upload_server())["upload_url"]
-    request = requests.post(upload_server, files={'file': open('chat_info.txt', 'rb')})
-    uploaded_document = eval(str((await message.ctx_api.docs.save(file=request.json()['file'], title='chat_info')).json()))
+    request = requests.post(upload_server, files={'file': open(file_name, 'rb')})
+    uploaded_document = eval(str((await message.ctx_api.docs.save(file=request.json()['file'], title=file_name)).json()))
     await message.ctx_api.messages.edit(
         peer_id=message.peer_id,
         attachment='doc' + str(uploaded_document['doc']['owner_id']) + '_' + str(uploaded_document['doc']['id']),
         message_id=message.id
     )
     await message.ctx_api.docs.delete(owner_id=uploaded_document['doc']['owner_id'], doc_id=uploaded_document['doc']['id'])
-    os.remove("chat_info.txt")
+    os.remove(file_name)
 
 # Записывает инфу о чате в файл
-async def write_in_file_conversation_info(message, t_info, null: int = 0, false: int = 0, true: int = 1):
-    file = open("chat_info.txt", "w", encoding="utf-8")
+async def write_in_file_conversation_info(message, t_info, file_name, null: int = 0, false: int = 0, true: int = 1):
+    file = open(file_name, "w", encoding="utf-8")
     t_all_member_ids = eval(t_info["preview"].json())["members"]
     t_member_ids = []
     t_group_ids = []
@@ -85,10 +84,7 @@ async def write_in_file_conversation_info(message, t_info, null: int = 0, false:
             t_member_ids.append(t_all_member_ids[counter])
         else:
             t_group_ids.append(t_all_member_ids[counter] * (-1))
-    file.write(str("Название беседы: " + str(eval(t_info["preview"].json())["title"]) + "\n\nID админа: " + str(
-        eval(t_info["preview"].json())["admin_id"]) + "\n\nКоличество всех участников: " + str(
-        eval(t_info["preview"].json())["members_count"]) + "\n\nКоличество людей: " + str(
-        len(t_member_ids)) + "\n\nКоличество сообществ: " + str(len(t_group_ids)) + "\n\nID пользователей: "))
+    file.write(str("Название беседы: " + (str(eval(t_info["preview"].json())["title"])).encode('utf-8', 'replace').decode() + "\n\nID админа: " + str(eval(t_info["preview"].json())["admin_id"]) + "\n\nКоличество всех участников: " + str(eval(t_info["preview"].json())["members_count"]) + "\n\nКоличество людей: " + str(len(t_member_ids)) + "\n\nКоличество сообществ: " + str(len(t_group_ids)) + "\n\nID пользователей: "))
     for counter in range(0, len(t_member_ids)):
         file.write(str(t_member_ids[counter]) + ", ")
     file.write("\n\nID сообществ: ")
@@ -103,26 +99,46 @@ async def write_in_file_conversation_info(message, t_info, null: int = 0, false:
     file.write("\n\nИмя и фамилия + (ID пользователя): ")
     t_member_names = await message.ctx_api.users.get(user_ids=t_member_ids)
     for counter in range(0, len(t_member_names)):
-        file.write(str(eval(t_member_names[counter].json())['first_name']) + " " + str(
-            eval(t_member_names[counter].json())['last_name']) + " (" + str(
-            eval(t_member_names[counter].json())['id']) + "), ")
+        file.write(str(eval(t_member_names[counter].json())['first_name']) + " " + str(eval(t_member_names[counter].json())['last_name']) + " (" + str(eval(t_member_names[counter].json())['id']) + "), ")
     file.write("\n\nНазвание + (ID сообщества): ")
     t_group_names = await message.ctx_api.groups.get_by_id(group_ids=t_group_ids)
     for counter in range(0, len(t_group_names)):
-        file.write(str(eval(t_group_names[counter].json())['name']) + " (" + str(
-            eval(t_group_names[counter].json())['id'] * (-1)) + "), ")
+        file.write(str(eval(t_group_names[counter].json())['name']) + " (" + str(eval(t_group_names[counter].json())['id'] * (-1)) + "), ")
     file.close()
-    await upload_and_edit_message_in_file(message)
+    await upload_and_edit_message_in_file(message, file_name)
 
 # Редактирует сообщение на txt файл, в котором находится инфа о беседе
 @user.on.chat_message(from_id = from_id_list, command = "get info")
 async def get_chat_info(message: Message, null: int = 0, false: int = 0, true: int = 1):
-    await write_in_file_conversation_info(message, dict(await message.ctx_api.messages.get_chat_preview(peer_id=message.peer_id)))
+    await write_in_file_conversation_info(message, dict(await message.ctx_api.messages.get_chat_preview(peer_id=message.peer_id)), "chat_info.txt")
 
 # Редактирует сообщение на txt файл, в котором находится инфа о беседе с ссылки
 @user.on.message(from_id = from_id_list, command = ("get info", 1))
 async def get_chat_info_by_link(message: Message, args: Tuple[str], none: int = 0, true: int = 1, null: int = 0, false: int = 0):
-    await write_in_file_conversation_info(message, dict(await message.ctx_api.messages.get_chat_preview(link=args[0])))
+    await write_in_file_conversation_info(message, dict(await message.ctx_api.messages.get_chat_preview(link=args[0])), "chat_info.txt")
+
+@user.on.message(from_id = from_id_list, text = ("/audio <audio>"))
+async def audio_message(message, audio: Optional[str] = None, null: int = 0, false: int = 0, true: int = 1):
+    try:
+        upload_server = dict(await message.ctx_api.docs.get_messages_upload_server(type = "audio_message", peer_id=message.peer_id))["upload_url"]
+        request = requests.post(upload_server, files={'file': open(('Audio\\' + audio + '.ogg'), 'rb')})
+        uploaded_document = eval(str((await message.ctx_api.docs.save(file=request.json()['file'], title=(audio + '.ogg'))).json()))
+        await message.ctx_api.messages.send(
+            random_id = 0,
+            peer_id=message.peer_id,
+            attachment='doc' + str(uploaded_document['audio_message']['owner_id']) + '_' + str(uploaded_document['audio_message']['id'])
+        )
+        await message.ctx_api.docs.delete(owner_id=uploaded_document['audio_message']['owner_id'], doc_id=uploaded_document['audio_message']['id'])
+        await message.ctx_api.messages.delete(message_ids = message.id, delete_for_all = 1, peer_id = message.peer_id)
+    except VKAPIError[15]:
+        await message.ctx_api.messages.delete(message_ids=message.id, peer_id=message.peer_id)
+    except FileNotFoundError:
+        await message.ctx_api.messages.edit(peer_id=message.peer_id,message=("Can't send audio, reason: No such file or directory: \'Audio\\" + audio + ".ogg\'"),message_id=message.id)
+        await asyncio.sleep(2.0)
+        try:
+            await message.ctx_api.messages.delete(message_ids=message.id, delete_for_all=1, peer_id=message.peer_id)
+        except VKAPIError[15]:
+            await message.ctx_api.messages.delete(message_ids=message.id, peer_id=message.peer_id)
 
 # Перезапускает бота
 @user.on.message(from_id = from_id_list, command = "restart")
@@ -138,7 +154,7 @@ async def show_help(message: Message):
         custom_command_list += command_list[i] + "\n"
     await message.ctx_api.messages.edit(
         peer_id=message.peer_id,
-        message="Кастомные команды для вызова функционала:\n\n" + custom_command_list + "\n" + "\nКоманды для вызова функционала:\n\n/get info - редактирует сообщение на txt файл, в котором находится список всех ID пользователей и групп в локальной беседе\n/get info <link> - редактирует сообщение на txt файл, в котором находится список всех ID пользователей и групп в беседе по ссылке приглашения\n/restart - перезапускает бота\n/help - выводит список со всеми командами",
+        message="Кастомные команды для вызова функционала:\n\n" + custom_command_list + "\n" + "\nКоманды для вызова функционала:\n\n/get info - редактирует сообщение на txt файл, в котором находится список всех ID пользователей и групп в локальной беседе\n/get info <link> - редактирует сообщение на txt файл, в котором находится список всех ID пользователей и групп в беседе по ссылке приглашения\n/audio <audio name> - отправляет голосовое сообщение.\n<audio name> - название файла в папке Audio в формате .ogg.\nОграничения: частота дискретизации — 16 кГц, битрейт — 16 кбит/с, длительность — не более 5 минут, моно.\n/restart - перезапускает бота\n/help - выводит список со всеми командами",
         message_id=message.id
     )
 
